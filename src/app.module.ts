ed4cfcd062d2ff0cdb61e4a3210291488fbb5494
sebastianaf <sebastianafanador95@gmail.com>
2023-09-12 08:44:37 -0500

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
+
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import environments from './config/environments';
+import { SeedModule } from './seed/seed.module';
+import { DatabaseModule } from './database/database.module';
+import config from './config';
+import validationSchema from './config/validation-schema';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || `.env`,
+      load: [config],
+      isGlobal: true,
+      validationSchema,
+    }),
+    UserModule,
+    AuthModule,
+    SeedModule,
+    DatabaseModule,
+  ],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class AppModule {}
